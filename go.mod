08587fef43aced3244b9f24e98f484e03af6baa1
zackyuan <35691489+zackyzg@users.noreply.github.com>
2024-11-11 14:12:55 +0800

    update

---
+++ go.mod
@@ -0,0 +1,24 @@
+module go_captcha_image_example
+
+go 1.23.2
+
+require (
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/mojocn/base64Captcha v1.3.6
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/image v0.13.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/mojocn/base64Captcha"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Load templates
+	t := &Template{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+	e.Renderer = t
+
+	// Route to serve the login page
+	e.GET("/login", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "login.html", nil)
+	})
+
+	// Route to generate captcha
+	e.GET("/captcha", func(c echo.Context) error {
+		driver := base64Captcha.NewDriverDigit(80, 240, 5, 0.7, 80)
+		cp := base64Captcha.NewCaptcha(driver, base64Captcha.DefaultMemStore)
+		id, b64s, answer, err := cp.Generate()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"message": "captcha generation failed"})
+		}
+		return c.JSON(http.StatusOK, map[string]string{"id": id, "captcha": b64s, "answer": answer})
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
